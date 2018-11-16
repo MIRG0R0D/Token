214dc39ae59fd098be1f639b48ae2015d99c56be
dima <dimadovg3@gmail.com>
2018-11-16 15:14:13 +0100

    second

--- src/main/java/Token/Token/App.java
+++ src/main/java/Token/Token/App.java
@@ -1,5 +1,10 @@
 package Token.Token;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,25 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello F*@#$ World!" );
+        FileSignature fs = new FileSignature();
+        //fs.sign(file, keyStoreUrl, heslo, alias, keyStoreType);
+        //fs.verify
+        //fs.signwithUSB
+        //fs.verify
+        
+        
+        
+        
+    }
+    private byte[] loadFile(String url) {
+        Path path = Paths.get(url);
+        byte[] file = null;
+        try {
+            file = Files.readAllBytes(path);
+        } catch (IOException e) {
+            System.exit(0);
+        }
+        return file;
     }
 }
